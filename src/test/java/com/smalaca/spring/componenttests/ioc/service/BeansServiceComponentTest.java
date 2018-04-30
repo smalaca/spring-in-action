34a7a5ab1e2be96e2463a06bd3f854071d04d620
smalaca <ab13.krakow@gmail.com>
2018-04-30 08:25:16 +0200

    Use correct BeanService in BeansServiceComponentTest

--- src/test/java/com/smalaca/spring/componenttests/ioc/service/BeansServiceComponentTest.java
+++ src/test/java/com/smalaca/spring/componenttests/ioc/service/BeansServiceComponentTest.java
@@ -8,6 +8,7 @@ import com.smalaca.spring.ioc.service.NotExistingBeanException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -20,6 +21,7 @@ import static org.assertj.core.api.Assertions.catchThrowable;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = ApplicationConfiguration.class)
 public class BeansServiceComponentTest {
+    @Qualifier("beansServiceWithSingletonMaster")
     @Autowired private BeansService beansService;
 
     @Test
